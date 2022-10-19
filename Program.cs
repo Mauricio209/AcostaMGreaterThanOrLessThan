d1c786333ff2b43b5a570dcde6d5c5324e6e60f8
Mauricio Acosta <bernardoz209@icloud.com>
2022-10-19 10:32:25 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Mauricio Acosta
+//10-18-22
+//Git Challange Greater or less than
+
+
+string playAgain = "yes";
+while (playAgain == "yes")
+{
+    Console.Clear();
+Console.WriteLine("Enter two numbers");
+string numberOne = Console.ReadLine();
+string numberTwo = Console.ReadLine();
+
+int numOne = Convert.ToInt32(numberOne);
+int numTwo = Convert.ToInt32(numberTwo);
+
+if ( numOne > numTwo ){
+    Console.WriteLine(numOne + " is greater then " + numTwo);
+}else if( numOne < numTwo ){
+    Console.WriteLine(numOne + " is less than " + numTwo);
+}else{
+ Console.WriteLine(numOne + " is equal to " + numTwo);
+
+
+}
+Console.WriteLine("Type yes to play again");
+
+playAgain = Console.ReadLine();
+}
